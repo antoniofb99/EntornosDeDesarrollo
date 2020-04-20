eb71b06ef525abfcd04263b7c77794373fc68169
antoniofb99 <55828214+antoniofb99@users.noreply.github.com>
2020-04-20 23:19:32 +0200

    Create EDnegocio.ps1

---
+++ EDnegocio.ps1
@@ -0,0 +1,6 @@
+﻿$prod1=Read-Host 'Introduzca el primer producto'
+$prod2=Read-Host 'Introduzca el segundo  producto'
+$prod3=Read-Host 'Introduzca el tercer producto'
+$prod4=Read-Host 'Introduzca el cuarto producto'
+$urlsp='https://trends.google.es/trends/explore?geo=ES&q='+$prod1+','+$prod2+','+$prod3+','+$prod4
+Start-Process chrome $urlsp
