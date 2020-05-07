26deced16331d56296d6f4782bd63a80b8370358
antoniofb99 <55828214+antoniofb99@users.noreply.github.com>
2020-05-07 18:13:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from instabot import Bot
 mi_bot = Bot()
+
 mi_bot.login(username='programacion1234',password='programacion12345')
-mi_bot.follow_followers('barrestaurantelaluna')
+
+
+list_id = mi_bot.get_user_followers('programacion1234')
+mi_bot.send_messages("Hola amigo",list_id)
+
+post =mi_bot.get_total_user_medias('barrestaurantelaluna')
+mi_bot.download_photos(post, folder='comentarios',save_description=True)
